e0f216b13fe80d1eb7ff2820245cbc8175a956d7
Maria <mnava@chicagotechacademy.org>
2014-05-01 14:39:25 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fortyweekexam::Application.config.secret_token = '592d9ab0f76766e2a1c9a5391de8b5826366926a2a31611bbea35fd3168a9dada4d6a8af98c4fa33e96c8746885e60401c09cd8f879599da713ba633a85ba39b'
